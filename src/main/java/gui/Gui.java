bdb384a82981111ae8afca3a07fdd24adc758388
timoa <timo-angst@bluewin.ch>
2020-12-19 17:45:05 +0100

    improved saving

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -59,8 +59,9 @@ public class Gui {
         drawPanel.setSize(screenSize.width - 200, screenSize.height - 100);
         drawPanel.setVisible(true);
 
-        jf.add(drawPanel);
+        //createButtons();
 
+        jf.add(drawPanel);
         jf.add(draw);
         jf.requestFocus();
         jf.setVisible(true);
@@ -71,5 +72,56 @@ public class Gui {
         width = jf.getWidth();
 
 
+    }
+    public void createButtons(){
+        JButton redButton = new JButton();
+        redButton.setBackground(Color.RED);
+        redButton.setLocation(200, 25);
+        redButton.setSize(50, 25);
+        redButton.setVisible(true);
+
+        JButton orangeButton = new JButton();
+        orangeButton.setBackground(Color.ORANGE);
+        orangeButton.setLocation(200, 60);
+        orangeButton.setSize(50, 25);
+        orangeButton.setVisible(true);
+
+        JButton yellowButton = new JButton();
+        yellowButton.setBackground(Color.YELLOW);
+        yellowButton.setLocation(265, 25);
+        yellowButton.setSize(50, 25);
+        yellowButton.setVisible(true);
+
+        JButton pinkButton = new JButton();
+        pinkButton.setBackground(Color.PINK);
+        pinkButton.setLocation(265, 60);
+        pinkButton.setSize(50, 25);
+        pinkButton.setVisible(true);
+
+        JButton greenButton = new JButton();
+        greenButton.setBackground(Color.GREEN);
+        greenButton.setLocation(330, 25);
+        greenButton.setSize(50, 25);
+        greenButton.setVisible(true);
+
+        JButton rectModeButton = new JButton();
+        greenButton.setBackground(Color.GRAY);
+        greenButton.setText("Rect Mode");
+        greenButton.setLocation(1350, 25);
+        greenButton.setSize(50, 25);
+        greenButton.setVisible(true);
+
+        redButton.addActionListener(e -> Draw.color = Color.RED);
+        orangeButton.addActionListener(e -> Draw.color = Color.ORANGE);
+        yellowButton.addActionListener(e -> Draw.color = Color.YELLOW);
+        pinkButton.addActionListener(e -> Draw.color = Color.PINK);
+        greenButton.addActionListener(e -> Draw.color = Color.GREEN);
+        rectModeButton.addActionListener(e -> KeyHandler.rectMode = true);
+
+        jf.add(redButton);
+        jf.add(orangeButton);
+        jf.add(yellowButton);
+        jf.add(pinkButton);
+        jf.add(greenButton);
     }
 }
